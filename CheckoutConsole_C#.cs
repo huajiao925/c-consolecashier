cf5da3380334b8b4849f4346ceabafd04c095d19
YT <61646691+huajiao925@users.noreply.github.com>
2020-05-03 15:25:09 +0800

    Update CheckoutConsole_C#.cs

--- CheckoutConsole_C#.cs
+++ CheckoutConsole_C#.cs
@@ -4,17 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace CheckoutConsole
 {
-    class SamplePaper
+    class CashierProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Input:");
-
             string moreItems = "";
             List<string> purchasedItems = new List<string> { };
-            
+            //takes in items and add it in to item list until No more item
             do
             {
                 Console.Write("Enter Item Code: ");
@@ -30,30 +28,31 @@ namespace ConsoleApp1
                 purchasedItems.AddRange(item);
             }
             while (moreItems == "Y");
-
+            //asking for membership and record membershipID
             Console.Write("The shopper a loyalty member?");
             string isMember = Console.ReadLine();
+            string membershipID = "";
             if (isMember == "Y")
             {
-                Console.WriteLine("Member No.: {0}", "KJ3525");
+                Console.WriteLine("What is the MembershipID: ");
+                membershipID = Console.ReadLine();
             }
-            
-            printReceipt(purchasedItems, isMember);
+            //call function to print the receipt
+            printReceipt(purchasedItems, isMember, membershipID);
         }
         
-        static void printReceipt(List<string> purchasedItems, string isMember)
+        static void printReceipt(List<string> purchasedItems, string isMember, string membershipID)
         {
             var dayofWeek = DateTime.Today.DayOfWeek;
             var today = DateTime.Today;
-
+            //print the store name and date
             Console.WriteLine("\t\t" + "ABC STORE\n" + "\t\t" + "SINGAPORE\n\n" + "\t\t" + "INVOICE\n");
             Console.WriteLine("Date of Pruchase: {0}, {1}", today, dayofWeek);
             Console.WriteLine("".PadLeft(50,'-'));
             Console.WriteLine("SNo"+ "\t" + "Item Code"+ "\t" + "Qty" + "\t" + "U/Price" + "\t" + "Cost" + "\t" + "Discount" + "Net");
             Console.WriteLine("".PadLeft(50, '-'));
 
-
-
+            //retrieve Items from List.
             double totalAmount = 0.0;
             int sNumber = 0;
             for (int i = 0; i < purchasedItems.Count(); i++) 
@@ -73,6 +72,7 @@ namespace ConsoleApp1
                 };
                 
                 double net = cost - midweekDiscount;
+                //print out each item and whether it's entitled for mid-week food discount
                 Console.WriteLine(
                     "{0}" + "\t"+
                     "{1}" + "\t\t" +
@@ -89,20 +89,19 @@ namespace ConsoleApp1
                     midweekDiscount, 
                     net);
 
-                 totalAmount += net;
-                    
+                 totalAmount += net;                   
             };
             };
             Console.WriteLine("".PadLeft(50, '-'));
             double memberDiscount = 0.0;
             double netTotal = totalAmount - memberDiscount;
 
-
+            //print the total, discount, GST
             Console.WriteLine("Gross Total:\t\t\t\t\t{0:0.00}", totalAmount);
             if (isMember == "Y")
             {
                 memberDiscount = 0.1 * totalAmount;
-                Console.WriteLine("Member Discount (Member No {0})\t\t\t\t\t-{1:c}", "KJ3525", memberDiscount);
+                Console.WriteLine("Member Discount (Member No {0})\t\t\t\t\t-{1:c}", membershipID, memberDiscount);
             };            ;
             Console.WriteLine("GST @ 7%\t\t\t\t\t+ {0:0.00}", netTotal * .07);
             Console.WriteLine("\nPlease Pay:\t\t\t\t\t{0:c}", netTotal * 1.07);
